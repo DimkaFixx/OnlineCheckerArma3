1b9893b5f1bfad0d19633cc16c52cc71568020e6
DimkaFixx <d.katyagin@yandex.ru>
2026-04-19 02:31:12 +0300

    Fix bugs

--- sheets.py
+++ sheets.py
@@ -39,7 +39,7 @@ class GoogleTableManager:
         self.sheet = self.spreadsheet.worksheet("Состав")
         rows_count = self.sheet.row_count
         cols_count = self.sheet.col_count
-        self.sheet.sort((1, 'asc'), range=f'{os.getenv("ROWSTOSORT_FROM")}:{os.getenv("ROWSTOSORT_TO")}{rows_count}')
+        self.sheet.sort((1, 'asc'), range=f'{os.getenv("ROWSTOSORT_FROM")}3:{os.getenv("ROWSTOSORT_TO")}{rows_count}')
         full_range = f"A1:{gspread.utils.rowcol_to_a1(rows_count, cols_count)}"
         border_style = {
             "style": "SOLID",
