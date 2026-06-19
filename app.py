1819359d8f0f56265855d5805ea144bc41e01247
DimkaFixx <d.katyagin@yandex.ru>
2026-06-19 19:36:01 +0300

    Fix bug of time

--- app.py
+++ app.py
@@ -20,6 +20,7 @@ def main():
     print(f"{now.strftime('%Y-%m-%d %H:%M:%S')} | Форматирование таблицы", flush=True)
 
     while True:
+        now = datetime.now(MSK_TZ)
         # Обновляем таблицу на :05, :15, :25, :35, :45, :55
         if now.minute % 10 == 5 and last_table_update_minute != now.minute:
             sheets_manager.update_table()
